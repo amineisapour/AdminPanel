6892b5769b339ce4376da33e83a9277af1b8bcad
Amin Eisapour <amin.eisapour@gmail.com>
2021-06-26 22:50:22 +0430

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//? material
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './layouts/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    DashboardComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    //? material
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public direction: string = 'menu';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Output() sideNavToggle = new EventEmitter();
+
+  toggle() {
+    this.sideNavToggle.emit();
+  }
+
+  mouseEnter() {
+    this.direction = 'menu_open';
+  }
+
+  mouseLeave() {
+    this.direction = 'menu';
+  }
+}
